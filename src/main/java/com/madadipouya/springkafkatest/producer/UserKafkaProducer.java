2af1af351de41e3b7d76a3a5c98d0c295883b997
kasramp <kasra_mp@yahoo.com>
2020-09-29 07:36:14 +0200

    Create a new topic with custom topic and replication factor

--- src/main/java/com/madadipouya/springkafkatest/producer/UserKafkaProducer.java
+++ src/main/java/com/madadipouya/springkafkatest/producer/UserKafkaProducer.java
@@ -1,7 +1,10 @@
 package com.madadipouya.springkafkatest.producer;
 
 import com.madadipouya.springkafkatest.dto.User;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +16,23 @@ public class UserKafkaProducer {
     @Value("${spring.kafka.topic.name}")
     private String topic;
 
+    @Value("${spring.kafka.replication.factor:1}")
+    private int replicationFactor;
+
+    @Value("${spring.kafka.partition.number:1}")
+    private int partitionNumber;
+
     public UserKafkaProducer(KafkaTemplate<String, User> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void writeToKafka(User user) {
         kafkaTemplate.send(topic, user.getUuid(), user);
+    }
+
+    @Bean
+    @Order(-1)
+    public NewTopic createNewTopic() {
+        return new NewTopic(topic, partitionNumber, (short) replicationFactor);
     }
 }
